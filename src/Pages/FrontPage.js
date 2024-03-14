7691cfb28ad7511147fbc6257260b7ddee6bf3c9
AnssiKuj <anssi.kujanpaa@gmail.com>
2024-03-14 13:48:14 +0200

    1.0
    Frontpage + css

--- src/Pages/FrontPage.js
+++ src/Pages/FrontPage.js
@@ -1,15 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 function FrontPage() {
-
-return (
-    <div>
-        <p>Testi</p>
+  return (
+    <div className='container'>
+      <div className='frontpage-container'>
+      </div>
+      <div className='frontpage-container2'>
+        <div className="header">
+          <h1>Taitaja9</h1>
+        </div>
+        <div className="links-container">
+            <div className="ajanotto-container">
+            <Link to="/Ajanotto" >
+                <p>Ajanotto</p>
+            </Link>
+            </div>
+            <div className="tulospalvelu-container">
+                <Link to="Tulospalvelu">
+                    <p>Tulospalvelu</p>
+                </Link>
+            </div>
+        </div>
+      </div>
     </div>
-
-    );
+  );
 }
-
 
 export default FrontPage;
