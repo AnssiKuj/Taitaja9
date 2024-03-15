8902135bc000971de146614b68ae2ac9c205ef8c
AnssiKuj <anssi.kujanpaa@gmail.com>
2024-03-15 15:50:22 +0200

    1.0
    Pieniä muutoksia kilpailijoitten lisäämiseen, sekä tyylittely

--- src/Pages/Timing.js
+++ src/Pages/Timing.js
@@ -1,27 +1,30 @@
 import React, { useState } from 'react';
+import "../css/contestant-container.css"
 
 function Timing() {
   const [contestants, setContestants] = useState([]);
 
   const addContestant = () => {
     if (contestants.length < 36) {
-      const newContestantId = contestants.length + 1;
-      setContestants(prevContestants => [...prevContestants, `Joukkue ${newContestantId}`]);
+      const contestantName = prompt("Syötä joukkueen nimi:");
+      if (contestantName) {
+        setContestants(prevContestants => [...prevContestants, contestantName]);
+      }
     }
   };
 
   return (
     <div className='container'>
       <div className='container1'>
-        <button onClick={addContestant}>Add Contestant</button>
-        <div className="grid-container">
-          {Array.from({ length: 36 }).map((_, index) => {
-            const contestant = contestants[index];
-            return (
-              <div key={index} className="grid-item">
-                {contestant ? contestant : ""}
-              </div>
-            );
+        <h2 className="header" onClick={addContestant}>Lisää kilpailija</h2>
+        <div className="kilpailija-container">
+        {Array.from({ length: 36 }).map((_, index) => {
+        const contestant = contestants[index];
+        return (
+          <div key={index} className="kilpailija-item">
+            {contestant ? contestant : ""}
+          </div>
+          );
           })}
         </div>
       </div>
