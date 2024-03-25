7a35e2540e2e4f57509d6aeb0476b9061d168106
AnssiKuj <anssi.kujanpaa@gmail.com>
2024-03-25 16:15:05 +0200

    database update
    Tietokantaan muutoksia,ehkä toimii

--- src/Pages/AddContestants.js
+++ src/Pages/AddContestants.js
@@ -3,53 +3,68 @@ import "../css/contestant-container.css"
 
 function Timing() {
 
+  // Tallentaa kilpailijoiden tiedot
   const [data, setData] = useState([])
 
   useEffect(()=> {
+    // Haetaan kilpailijoiden tiedot palvelimelta
     fetch('http://localhost:8081/joukkueet')
     .then(res => res.json())
     .then(data => setData(data))
     .catch(err => console.log(err));
   }, [])
 
+
   const [contestants, setContestants] = useState([]);
 
+  // Funktio, joka lisää uuden kilpailijan tietokantaan
   const addContestant = () => {
     if (contestants.length < 36) {
       const contestantName = prompt("Syötä joukkueen nimi:");
       if (contestantName) {
-        setContestants(prevContestants => [...prevContestants, contestantName]);
+        fetch('http://localhost:8081/insertContestant', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ JoukkueNimi: contestantName }),
+        })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          // Päivittää listan kilpailijoista
+          return fetch('http://localhost:8081/joukkueet');
+        })
+        .then(res => res.json())
+        .then(data => {
+          // Päivitetään data
+          setData(data);
+        })
+        .catch(error => {
+          console.error('Virhelisätessä kilpailijaa', error);
+          alert('Virhe lisätessä kilpailijaa.');
+        });
       }
     }
   };
+  
 
   return (
     <div className='container'>
       <div className='container1'>
+        {/* Lisää kilpaili clikkaamalla otsikkoa */}
         <h2 className="header" onClick={addContestant}>Lisää kilpailija</h2>
         <div className="kilpailija-container">
-          {contestants.map((contestant, index) => (
-            <div key={index} className="kilpailija-item">
-              {contestant}
+          {/* Kilpailijoiden tiedot */}
+          {data.map((d, i) => (
+            <div key={i} className="kilpailija-item">
+              {d.JoukkueNimi}
             </div>
           ))}
         </div>
       </div>
       <div className='container2'>
-            <table>
-              <thead>
-                <th>Nimi</th>
-                <th>Aika</th>
-              </thead>
-              <tbody>
-                {data.map((d ,i) =>(
-                  <tr key={i}>
-                    <td>{d.JoukkueNimi}</td>
-                    <td>{d.JoukkueAika}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
       </div>
     </div>
   );
